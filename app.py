32d5deb4a9a5d0e768dff215fe8eb40b0c13a079
Andrew Hyder <hyd415@gmail.com>
2013-08-30 16:46:47 -0700

    Parse!

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, url_for
 from flask.ext.heroku import Heroku
-# import requests
+import requests
 
 #----------------------------------------
 # initialization
@@ -9,18 +9,31 @@ from flask.ext.heroku import Heroku
 app = Flask(__name__)
 heroku = Heroku(app)  # Sets CONFIG automagically
 
+app.config.update(
+    X_Parse_Application_Id = environ.get('X-Parse-Application-Id'),
+    X_Parse_REST_API_Key = environ.get('X-Parse-REST-API-Key')
+)
+
 #----------------------------------------
 # Routes
 #----------------------------------------
 
 @app.route('/')
 def index():
-    f = open('static/death_switch_message.txt','r')
-    death_switch_message = f.read()
-    f.close()
-    f = open('static/death_texts.txt','r')
-    death_texts = f.readlines()
-    f.close()
+    headers = {
+        "X-Parse-Application-Id" : X_Parse_Application_Id,
+        "X-Parse-REST-API-Key" : X_Parse_REST_API_Key
+    }
+    r = requests.get("https://api.parse.com/1/classes/DeathSwitchMessage", headers=headers)
+    r = r.json()
+    death_switch_message = r['results'][0]['text']
+    
+    r = requests.get("https://api.parse.com/1/classes/DeathTexts", headers=headers)
+    r = r.json()
+    r = r['results']
+    death_texts = []
+    for i in r:
+        death_texts.append(i['text'])
     return render_template('index.html', death_switch_message=death_switch_message, death_texts=death_texts)
 
 if __name__ == '__main__':
