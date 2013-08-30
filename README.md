70fc71a3692b40338149ff084e8c825517e1580d
Andrew Hyder <hyd415@gmail.com>
2013-08-30 15:25:49 -0700

    Website mockup.

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ after-the-tone
 ==============
 
 An exploration of the digital here after.
+
+## Installation
+1. Create a Virtual Environment `virtualenv .venv`
+2. Activate it `source .venv/bin/activate`
+3. Install the requirements. `pip install -r requirements.txt`
---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, url_for
+# from flask.ext.heroku import Heroku
+# import requests
+
+#----------------------------------------
+# initialization
+#----------------------------------------
+
+app = Flask(__name__)
+# heroku = Heroku(app)  # Sets CONFIG automagically
+
+#----------------------------------------
+# Routes
+#----------------------------------------
+
+@app.route('/')
+def index():
+    f = open('static/death_switch_message.txt','r')
+    death_switch_message = f.read()
+    f.close()
+    f = open('static/death_texts.txt','r')
+    death_texts = f.readlines()
+    f.close()
+    return render_template('index.html', death_switch_message=death_switch_message, death_texts=death_texts)
+
+if __name__ == '__main__':
+    app.run(debug=True)
